7dc776b216d1e51da0e1bfdd6be6d1c741e6611f
Mage Alexstra <36608070+magealexstra@users.noreply.github.com>
2019-10-19 14:25:27 -0500

    first commit of SQLquerydb

---
+++ src/SQLquerydb.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from difflib import get_close_matches as gcm
+
+con = mysql.connector.connect(
+    user="ardit700_student",
+    password="ardit700_student",
+    host="108.167.140.122",
+    database="ardit700_pm1database"
+)
+
+cursor = con.cursor()
+
+word = input("Enter a word: ")
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
+
+results = cursor.fetchall()
+
+
+if results:
+    for result in results:
+        print(results)
+else:
+    print("word not present!")
